f20dc2ff9efe1d95571bb8fc18e185e35167138e
Kevin Hernon <khernon1@gmail.com>
2016-11-14 16:43:34 -0500

    fix services columns

--- homepage/app/assets/javascripts/add_services.js
+++ homepage/app/assets/javascripts/add_services.js
@@ -6,24 +6,35 @@
     method: "GET",
     url: "http://localhost:3000/taglines",
   }).done(function(response) {
-    let servicesInfo = response[Math.floor(Math.random() * response.length)];
-    let servicesList = [
-      {title: "Unique Layouts", data: response[Math.floor(Math.random() * response.length)]},
-      {title: "Responsive", data: response[Math.floor(Math.random() * response.length)]},
-      {title: "Ajax Transitions", data: response[Math.floor(Math.random() * response.length)]},
-      {title: "E-Commerce", data: response[Math.floor(Math.random() * response.length)]},
-      {title: "Unlimited Portfolios", data: response[Math.floor(Math.random() * response.length)]},
-      {title: "Powerful", data: response[Math.floor(Math.random() * response.length)]}
-    ]
-
-    servicesList.forEach(function(service, index) {
-      $("#services-data").append("<div class='col-xs-4'><p>" + service["title"] +
-        "</p><p>" + service["data"].saying + "</p></div>")     
-    })
+   let servicesInfo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+     let servicesList = [
+       {title: "Unique Layouts", data: servicesInfo},
+       {title: "Responsive", data: servicesInfo},
+       {title: "Ajax Transitions", data: servicesInfo},
+       {title: "E-Commerce", data: servicesInfo},
+       {title: "Unlimited Portfolios", data: servicesInfo},
+        {title: "Powerful", data: servicesInfo}
+     ]
+ 
+     servicesList.forEach(function(service, index) {
+       $("#services-data").append("<p>" + service["title"] +
+         "<ul>" + service["data"] + "</ul></p>") 
+     })
   })
   });
 })(jQuery);
 
+    // let n = 1
+    // footerList.forEach(function(footer, index) {      
+    //   $("#footer-data" + n).append("<p id='title" +index + "'>" + footer["title"] +
+    //     "</p>")
+    //   footer["data"].forEach(function(data) {
+    //     $("#footer-data" + n + " #title" + index).append("<ul>" + data + "</ul>")
+    //   })
+    //   if (index % 2 != 0) {
+    //     n++
+    //   }
+    // })
       //     $('table #header').append("<td id='title" + index + "'>" + service["title"] +
       //   "</td>")
       // $('table #data').append("<td id='data" + index + "'>" + service["data"].saying +
