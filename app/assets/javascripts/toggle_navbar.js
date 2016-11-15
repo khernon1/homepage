30c93da75f94069f51837142ad6de135ac432804
Kevin Hernon <khernon1@gmail.com>
2016-11-14 20:03:15 -0500

    last update

--- app/assets/javascripts/toggle_navbar.js
+++ app/assets/javascripts/toggle_navbar.js
@@ -3,15 +3,15 @@
     
     // Toggle navigation
     $('#nav-toggle').click(function() {
+      event.preventDefault()
       this.classList.toggle("active");
-      // If sidebar is visible:
-      // if ($('body').hasClass('show-nav')) {
-      //   // Hide sidebar
-      //   $('body').removeClass('show-nav');
-      // } else { // If sidebar is hidden:
-        $('body').addClass('show-nav');
-        // Display sidebar
-      }
-    );
+    // If sidebar is visible:
+      if ($('body').hasClass('show-nav')) {
+        // Hide sidebar
+          $('body').removeClass('show-nav');
+      } else { // If sidebar is hidden:        
+          $('body').addClass('show-nav'); // Display sidebar
+      }        
+    });    
   });
 })(jQuery);
